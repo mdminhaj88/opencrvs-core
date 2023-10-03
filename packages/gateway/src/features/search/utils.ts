c865fc7729044d9967c810c0d74c5393979abf0b
Riku Rouvila <riku.rouvila@gmail.com>
2023-10-03 12:47:16 +0300

    fix IAuthHeader import

--- packages/gateway/src/features/search/utils.ts
+++ packages/gateway/src/features/search/utils.ts
@@ -9,9 +9,10 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-import { IAuthHeader } from '@gateway/common-types'
+
 import { SEARCH_URL } from '@gateway/constants'
 import { GQLAdvancedSearchParametersInput } from '@gateway/graphql/schema'
+import { IAuthHeader } from '@opencrvs/commons'
 import fetch from 'node-fetch'
 
 export interface ISearchCriteria {
