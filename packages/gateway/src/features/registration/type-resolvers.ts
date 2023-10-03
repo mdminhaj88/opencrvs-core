a157d422dd85ee59c8569f1ac81a3e7babf5549f
Riku Rouvila <riku.rouvila@gmail.com>
2023-10-03 16:31:49 +0300

    deprecate valueString field for age

--- packages/gateway/src/features/registration/type-resolvers.ts
+++ packages/gateway/src/features/registration/type-resolvers.ts
@@ -185,7 +185,13 @@ export const typeResolvers: GQLResolver = {
         `${OPENCRVS_SPECIFICATION_URL}extension/age`,
         person.extension
       )
-      return (marriageExtension && marriageExtension.valueString) || null
+      if (!marriageExtension) {
+        return null
+      }
+      if (marriageExtension.valueInteger) {
+        return marriageExtension.valueInteger
+      }
+      return marriageExtension.valueString
     },
     maritalStatus: (person) => {
       return person && person.maritalStatus && person.maritalStatus.text
@@ -343,7 +349,13 @@ export const typeResolvers: GQLResolver = {
         `${OPENCRVS_SPECIFICATION_URL}extension/age`,
         person?.extension || []
       )
-      return (marriageExtension && marriageExtension.valueString) || null
+      if (!marriageExtension) {
+        return null
+      }
+      if (marriageExtension.valueInteger) {
+        return marriageExtension.valueInteger
+      }
+      return marriageExtension.valueString
     },
     birthDate: async (
       relatedPerson,
